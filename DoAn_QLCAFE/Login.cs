35fe6281a3c1482084bcc3db188c23106bd7aaba
PhucNguyen155 <phucnguyenhuu155@gmail.com>
2020-08-06 00:02:13 +0700

    a

---
+++ DoAn_QLCAFE/Login.cs
@@ -0,0 +1,92 @@
+﻿using DoAn_QLCAFE;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAn_QLCAFE
+{
+    
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        QLCAFEDataContext qlcf = new QLCAFEDataContext();
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            List<QL_NguoiDung> ttk = getTenDN();
+            if (string.IsNullOrEmpty(txtUsername.Text) || string.IsNullOrEmpty(txtPassword.Text))
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin đăng nhập", "Thông báo", MessageBoxButtons.OK);
+            else if (!kiemTraDangNhap())
+            {
+                return;
+            }
+            else
+            {
+                FrmMain main = new FrmMain();
+                main.Show();
+                this.Hide();
+            }
+        }
+        public List<QL_NguoiDung> getTenDN()
+        {
+            var getTDN = from tdn in qlcf.QL_NguoiDungs select tdn;
+            return getTDN.ToList<QL_NguoiDung>();
+        }
+
+        public bool kiemTraDangNhap()
+        {
+            List<QL_NguoiDung> listTK = getTenDN();
+            string tendn = txtUsername.Text;
+            string matkhau = txtPassword.Text;
+            QL_NguoiDung taikhoan = null;
+            foreach (QL_NguoiDung tk in listTK)
+            {
+                if (tk.TenDangNhap.Equals(tendn))
+                {
+                    taikhoan = new QL_NguoiDung();
+                    taikhoan = tk;
+                    break;
+                }
+            }
+            if (taikhoan == null)
+            {
+                MessageBox.Show("Tên đăng nhập không tồn tại.", "Thông báo", MessageBoxButtons.OK);
+                return false;
+            }
+            else
+            {
+                if (taikhoan.MatKhau.Equals(matkhau))
+                {
+                    //loaiTaiKhoan = taikhoan.LoaiTaiKhoan;
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("Mật khẩu không đúng.", "Thông báo", MessageBoxButtons.OK);
+                    return false;
+                }
+            }
+        }
+
+        public bool kiemTraTaiKhoan(string tenDN)
+        {
+            QL_NguoiDung tk = qlcf.QL_NguoiDungs.Where(t => t.TenDangNhap == tenDN).FirstOrDefault();
+            if (tk != null)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ DoAn_QLCAFE/Login.designer.cs
@@ -0,0 +1,148 @@
+﻿namespace DoAn_QLCAFE
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.ckbShowchars = new System.Windows.Forms.CheckBox();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ckbShowchars
+            // 
+            this.ckbShowchars.AutoSize = true;
+            this.ckbShowchars.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckbShowchars.Location = new System.Drawing.Point(121, 173);
+            this.ckbShowchars.Margin = new System.Windows.Forms.Padding(4);
+            this.ckbShowchars.Name = "ckbShowchars";
+            this.ckbShowchars.Size = new System.Drawing.Size(144, 23);
+            this.ckbShowchars.TabIndex = 21;
+            this.ckbShowchars.Text = "Hiển thị mật khẩu";
+            this.ckbShowchars.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Image = ((System.Drawing.Image)(resources.GetObject("btnThoat.Image")));
+            this.btnThoat.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnThoat.Location = new System.Drawing.Point(291, 217);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnThoat.Size = new System.Drawing.Size(93, 39);
+            this.btnThoat.TabIndex = 20;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangNhap.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnDangNhap.Location = new System.Drawing.Point(132, 217);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(93, 39);
+            this.btnDangNhap.TabIndex = 19;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = false;
+            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(192, 129);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(192, 20);
+            this.txtPassword.TabIndex = 18;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(192, 80);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(192, 20);
+            this.txtUsername.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(83, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 18);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Mật khẩu :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 18);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Tài khoản :";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(478, 293);
+            this.Controls.Add(this.ckbShowchars);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox ckbShowchars;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
